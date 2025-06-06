c75f45b8c42d6cf80e1a967a71d5fd7106ec3d68
Oscar Harrison <oscar.d.harrison@gmail.com>
2025-06-06 02:14:30 -0300

    Fix email template and close sendmail block

--- index.js
+++ index.js
@@ -185,8 +185,8 @@ attachments: [
     content: pdfData,
   },
   {
-    filename: 'logo.png',
-    path: './logo.png', // this is correct based on your screenshot
+    filename: 'logo.jpg',
+    path: '\assets\logo.jpg', // this is correct based on your screenshot
     cid: 'logo'
   }
 ]
